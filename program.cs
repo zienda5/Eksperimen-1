4c1293102e1df748dfa3044f4876e70ca98c88f4
luthfi27anwar <mmdb_github@yahoo.com>
2013-01-18 13:06:06 -0800

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MultiFaceRec
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new mainForm());
+        }
+    }
+}
