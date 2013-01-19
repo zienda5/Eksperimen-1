40a1bf9a4925fbd7346f5608e3f2d523e0efcdaa
Aji Rachmat Iksani <aji_github@yahoo.com>
2013-01-19 09:49:04 +0700

    Update mainForm.cs
    kompresi image fix

--- mainForm.cs
+++ mainForm.cs
@@ -9,15 +9,18 @@ using System.Windows.Forms;
 using Emgu.CV.Util;
 using Emgu.CV;
 using Emgu.CV.Structure;
+using System.Diagnostics;
 
 namespace MultiFaceRec
 {
     public partial class mainForm : Form
     {
-       
-        Image<Bgr, Byte> currentFrame;
-        Capture grabber;
+        public ImageProcessor imagesProcessor = new ImageProcessor();
 
+        public Image<Bgr, Byte> currentFrame;
+        public Capture grabber;
+        
+       
         public mainForm()
         {
             InitializeComponent();
@@ -46,6 +49,57 @@ namespace MultiFaceRec
             imageBoxFrameGrabber.Image = currentFrame;
             DateTime endTime = DateTime.Now;
             textBox2.Text = (endTime - StarTime).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Menginisialisasi variable capture untuk menampung image yang diambil
+            grabber = new Capture();
+            grabber.QueryFrame();
+            //Menginisialisasi variable FrameGraber untuk mengambil tiap frame dari webcam
+            Application.Idle -= new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            imagesProcessor.ApplySepia(20);
+            pictureBox1.Image = imagesProcessor.GetImage();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            imagesProcessor.ApplyEmboss(4);
+            pictureBox1.Image = imagesProcessor.GetImage();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            imagesProcessor.ApplyInvert();
+            pictureBox1.Image = imagesProcessor.GetImage();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            imagesProcessor.ApplyGreyscale();
+            pictureBox1.Image = imagesProcessor.GetImage();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "c:\\";
+            saveFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp/*.jpg";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (DialogResult.OK == saveFileDialog.ShowDialog())
+            {
+                pictureBox1.Image = imagesProcessor.GetImage();
+                pictureBox1.Image.Save(saveFileDialog.FileName);
+                // m_Bitmap.Save(saveFileDialog.FileName);
+            }
         }
 
     }
